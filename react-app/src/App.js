5c11c82ec8634df07f112f901b12e6c1b9919471
elvenarjun <elvenarjun@gmail.com>
2021-11-03 22:39:53 +1300

    delete tasks functionality added.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,38 @@
 import Header from './components/Header'
-import Tasks from './components/Tasks';
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks,setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appoitment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 3:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Shopping',
+        day: 'Feb 7th at 4:30pm',
+        reminder: false,
+    }
+])
+
+//Delete a task using FaTimes 'x' icon
+const deleteTask = (id) =>{
+  setTasks(tasks.filter((task)=> task.id !== id));
+}
+
   return (
     <div className="container">
         <Header title='Task Tracker'/>
-        <Tasks />
+        {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : "No Tasks left"}
     </div>
   );
 }
--- react-app/src/components/Tasks.js
+++ react-app/src/components/Tasks.js
@@ -1,30 +1,11 @@
 import React from 'react'
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appoitment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 3:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Shopping',
-        day: 'Feb 7th at 4:30pm',
-        reminder: false,
-    }
-]
+import Task from './Task'
 
-const Tasks = () => {
+const Tasks = ({tasks,onDelete}) => {
     return (
        <>
        {tasks.map((task) => (
-       <h3 key={task.id}>{task.text}</h3>
+       <Task key={task.id} task={task} onDelete={onDelete}></Task>
        ))}
        </>
     )
