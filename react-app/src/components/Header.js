5eebdb8c2f00d996f65d8338e812920f7a2152e0
elvenarjun <elvenarjun@gmail.com>
2021-11-03 22:59:52 +1300

    toggle style for reminder booleans

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
-import Tasks from './Tasks';
+
 const onClick = () => {
   console.log('click');
 }
--- react-app/src/components/Task.js
+++ react-app/src/components/Task.js
@@ -3,7 +3,7 @@ import { FaTimes } from 'react-icons/fa'
 
 const Task = ({task,onDelete}) => {
     return (
-        <div className='task'>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`}>
             <h3>{task.text} <FaTimes onClick={()=>onDelete(task.id)} styles={{ color: 'red', cursor: 'pointer' }} />
             </h3>
             <p>{task.day}</p>
